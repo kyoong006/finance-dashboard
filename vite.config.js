52263071268a5ef69244349933b7425beb6b0afb
kyoong006 <yoongkv@gmail.com>
2024-12-16 16:52:50 +0800

    Update Files
    update files

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/finance-dashboard/', // Replace with your repository name
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: 'src/setupTests.js'
+  }
 });
